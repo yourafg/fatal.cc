9d2f2e5517882ffa193b863117fe7c5625d85113
yourafg <elyassha.afg@gmail.com>
2025-05-20 20:20:26 -0400

    Update 104077217450185.lua

--- Games/104077217450185.lua
+++ Games/104077217450185.lua
@@ -75,7 +75,7 @@ local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
 MenuGroup:AddButton('Unload', function() Library:Unload() end)
 MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
 
-Library.ToggleKeybind = Options.MenuKeybind
+Library.ToggleKeybind = MenuGroup.Options.MenuKeybind
 ThemeManager:SetLibrary(Library)
 SaveManager:SetLibrary(Library)
 
