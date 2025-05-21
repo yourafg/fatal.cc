dcb3265109991e5204a0645cb7440a1a9b51af92
yourafg <elyassha.afg@gmail.com>
2025-05-21 06:35:57 -0400

    Update loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 104077217450185 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yourafg/fatal/refs/heads/main/Games/104077217450185.lua"))()
+else
+    print("Not a valid game")
+end
